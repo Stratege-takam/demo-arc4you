97d49d3e3e9366fad495c2b89e7e06a05e39f76f
Suffo Takam Danick (EXT) <TE8527@Belgrid.net>
2023-10-03 10:51:58 +0200

    feat(Demo3): Remove bad project

--- BE/Core/DemoPCBE99925.Core.Business/Common/Validators/IValidator.cs
+++
@@ -1,7 +0,0 @@
-
-namespace EG.DemoPCBE99925.Core.Business.Common.Validators;
-
-public interface IValidator
-{
-    void Validate();
-}
--- BE/Core/DemoPCBE99925.Core.IBusiness/Logic/IEnvironmentInfoBL.cs
+++
@@ -1,8 +0,0 @@
-using EG.DemoPCBE99925.Core.Domain;
-
-namespace EG.DemoPCBE99925.Core.IBusiness;
-
-public interface IEnvironmentInfoBL
-{
-    Task<EnvironmentInfo> GetEnvironmentInfoAsync();
-}
