438b08574f3a40cd45a6b4e1e9cc67fe8e2ce60f
Suffo Takam Danick (EXT) <TE8527@Belgrid.net>
2023-10-05 16:13:46 +0200

    feat(demo12): fix menu

--- FE/ManageCourse.Web/DemoPCBE99925.ManageCourse.Web/States/TeacherState.cs
+++ FE/ManageCourse.Web/DemoPCBE99925.ManageCourse.Web/States/TeacherState.cs
@@ -1,4 +1,5 @@
 using Arc4u.Dependency.Attribute;
+using EG.DemoPCBE99925.ManageCourse.Web.Proxies;
 using EG.DemoPCBE99925.ManageCourseService.Facade.Sdk;
 
 namespace EG.DemoPCBE99925.ManageCourse.Web.States;
@@ -6,14 +7,8 @@ namespace EG.DemoPCBE99925.ManageCourse.Web.States;
 [Export, Shared]
 public class TeacherState: BaseState
 {
-    private readonly ITeacherClient _client;
     public IList<TeacherDto> Teachers { get; set; }
 
-
-    public TeacherState(ITeacherClient teacherClient)
-    {
-        _client = teacherClient;
-    }
 
     public void RefreshList()
     {
